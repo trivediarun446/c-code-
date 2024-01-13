58e454a725f3534eceed4c21828969a82ecbd69f
Arun Trivedi <155215034+trivediarun446@users.noreply.github.com>
2024-01-14 00:42:50 +0530

    Create binarysearching.c

---
+++ binarysearching.c
@@ -0,0 +1,13 @@
+// write a c programe to search a element by binary searching 
+#include <stdio.h>
+int main(){
+  int n,flag=0;
+printf("enter the number of element in an array");
+  scanf("%d",&n);
+int arr[n];
+printf("enter the element in the array");
+for(int i:i<n;i++){
+printf("arr[%d]",i);
+scanf("%d",&arr[i]);
+for(int i=0;i<
+
